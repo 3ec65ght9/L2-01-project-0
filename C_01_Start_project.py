50f41e85c39dbd1f3a7f52e900ab9a12fcdd9a90
3ec65ght9 <govendera4795@masseyhigh.school.nz>
2025-02-14 13:08:47 +1300

    flexible multi line start project

---
+++ C_01_Start_project.py
@@ -0,0 +1,10 @@
+# Functions go here
+def start_project(start: object, decoration):
+    """Emphasises headings by adding decoration
+    at the start and end"""
+
+    print(f"{decoration * 3} {start} {decoration * 3}")
+
+
+# Main Routine goes here
+start_project("Programming is Fun!","=")
---
+++ C_01_Start_project_v3.py
@@ -0,0 +1,27 @@
+# Functions go here
+def start_project(decoration, start: object, lines=1):
+    """Create headings (3 lines), subheadings (2 lines) and emphasised
+    text / mini-headings (1 line). Only use emoji for single line statements"""
+
+    middle = f"{decoration * 3} {start} {decoration * 3}"
+    top_bottom = decoration * len(middle)
+
+    if lines == 1:
+        print(middle)
+    elif lines == 2:
+        print(middle)
+        print(top_bottom)
+
+    else:
+        print(top_bottom)
+        print(middle)
+        print(top_bottom)
+
+
+# Main Routine goes here
+start_project("Programming is Fun!", 'decoration "=", lines: 3')
+print()
+start_project("programming is Still Fun!", 'decoration "*", lines: 2')
+print()
+start_project("Emoji in Action",  'decoration "🐍", lines: 1')
+
